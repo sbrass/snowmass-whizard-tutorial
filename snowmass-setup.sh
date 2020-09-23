905ce88a671fc040ae9bc823439ed21da2fe3d24
Simon Brass <simon.brass@desy.de>
2020-09-23 14:38:27 +0200

    Add setup script; UFO model

---
+++ snowmass-setup.sh
@@ -0,0 +1,4 @@
+module use /local-scratch/software/modulefiles/
+module load gcc-8.2.0
+export LD_LIBRARY_PATH=/local-scratch/software/ee_gen/./packages/OpenLoops/lib:$LD_LIBRARY_PATH
+export PATH=/local-scratch/software/ee_gen/bin:$PATH
